5e7f48d6ac0ae45748275ad7f9e03340e17bb13f
Shoyebaktar Shirol <shoyebaktarshirol@gmail.com>
2024-02-16 19:37:29 +0530

    Python Practice

---
+++ Variables and Data Types.py
@@ -0,0 +1,75 @@
+print("HEllo WOrld")#Comment in Python
+print(33+44)
+name = "Shoyebaktar Shirol "
+age = 22
+college = "Kle society's Collge of Bca Gokak"
+print("My Name is :",name)
+print("My Age is :",age)
+print("I am Studying in ",college)
+
+
+a=10
+b=20
+print(a+b)
+
+#operators Arithmetic
+a=5
+b=5
+print("Addition:",a+b)
+print("Substarction",a-b)
+print("Multiplication",a*b)
+print("Division",a/b)
+print("Modulus:",a%b)
+
+#Relational Operators
+x=10
+y=20
+print(x==y)
+print(x<y)
+print(x>y)
+print(x!=y)
+print(x<=y)
+print(x>=y)
+
+#Assignment operator
+a=20
+a=a+20
+print(a)
+
+
+#Logical Operators
+val1=True
+val2=False
+print("and Operator :",val1 and val2)
+print("Or Operator", val1 or val2)
+print("Not Operator",not val1)
+
+#type Conversion
+a=6
+b=4.5
+print(int(b)) #converting float to int
+sum=a+b
+print(int(sum))  #Converting sum to integer
+
+
+# #Input in Python
+# name  = input("Enter Your Name :")
+# print("Welcome to Python ",name)
+# age = int(input("Enter Your Age:"))
+# sgpa = float(input("Enter Your SGPA:"))
+# print("Your age is :",age)
+# print("Your SGPA is :",sgpa)
+
+
+# num1 = int(input("Enter The Frist Number:"))
+# num2 = int(input("Enter The Second Number:"))
+# print("Sum Of Two Numbers Are ",num1+num2)
+
+
+# num1 = float(input("Enter The Frist Number:"))
+# num2 = int(input("Enter The Second Number:"))
+# print("Average is  = ",(num1+num2)/2)
+
+num1 = int(input("Enter The First Number: "))
+num2 = int(input("Enter The Second Number: "))
+print(num1>=num2)
