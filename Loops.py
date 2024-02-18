16e52c856837082cb11cfd0a1398cd87acb819c7
Shoyebaktar Shirol <shoyebaktarshirol@gmail.com>
2024-02-18 20:15:10 +0530

    Loops

---
+++ Loops.py
@@ -0,0 +1,49 @@
+# i = 1
+# while(i <= 5):
+#     print("Hello World")
+#     i += 1
+# print(i)
+
+
+# Practice of loops 
+# i=0
+# while(i<=):
+#     print(i)
+#     i +=1
+  
+
+# i = 100
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+# n = int(input("Enter The Number To Multiply:"))
+# i = 1
+# while i <= 10:
+#     print(n * i)
+#     i += 1 
+# print(i)
+
+
+# n = [1,4,9,16,25,36,49,64,81,100]
+# idx = 0
+# while(idx < len(n)):
+#     print(n[idx])
+#     idx += 1
+
+
+#Finding the x element in the tuple
+nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
+x = 55
+i = 0
+while i < len(nums):
+    if nums[i] == x:
+        print("Element Found At The Index Of:", i)
+        break
+    i += 1
+else:
+    print("The Element You are Searching for the element was Not Found in the Tuple. Please Enter a Valid Number.",x)
+
+
+   
+
