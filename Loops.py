dc49f6604ab5c7c0a2e39c2b243b91813b38edc7
Shoyebaktar Shirol <shoyebaktarshirol@gmail.com>
2024-02-29 11:35:16 +0530

    Practice

--- Loops.py
+++ Loops.py
@@ -1,49 +1,70 @@
-# i = 1
-# while(i <= 5):
-#     print("Hello World")
-#     i += 1
-# print(i)
+# # i = 1
+# # while(i <= 5):
+# #     print("Hello World")
+# #     i += 1
+# # print(i)
 
 
-# Practice of loops 
-# i=0
-# while(i<=):
-#     print(i)
-#     i +=1
+# # Practice of loops 
+# # i=0
+# # while(i<=):
+# #     print(i)
+# #     i +=1
   
 
-# i = 100
-# while i >= 1:
-#     print(i)
-#     i -= 1
+# # i = 100
+# # while i >= 1:
+# #     print(i)
+# #     i -= 1
 
-# n = int(input("Enter The Number To Multiply:"))
-# i = 1
-# while i <= 10:
-#     print(n * i)
-#     i += 1 
-# print(i)
-
-
-# n = [1,4,9,16,25,36,49,64,81,100]
-# idx = 0
-# while(idx < len(n)):
-#     print(n[idx])
-#     idx += 1
+# # n = int(input("Enter The Number To Multiply:"))
+# # i = 1
+# # while i <= 10:
+# #     print(n * i)
+# #     i += 1 
+# # print(i)
 
 
-#Finding the x element in the tuple
-nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
-x = 55
-i = 0
-while i < len(nums):
-    if nums[i] == x:
-        print("Element Found At The Index Of:", i)
-        break
-    i += 1
-else:
-    print("The Element You are Searching for the element was Not Found in the Tuple. Please Enter a Valid Number.",x)
+# # n = [1,4,9,16,25,36,49,64,81,100]
+# # idx = 0
+# # while(idx < len(n)):
+# #     print(n[idx])
+# #     idx += 1
 
 
-   
+# #Finding the x element in the tuple
+# nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
+# x = 55
+# i = 0
+# while i < len(nums):
+#     if nums[i] == x:
+#         print("Element Found At The Index Of:", i)
+#         break
+#     i += 1
+# else:
+#     print("The Element You are Searching for the element was Not Found in the Tuple, Please Enter a Valid Number.",x)
+
+
+
+
+# def reverse_list(s):
+#     return s[::-1]
+# my_list = [1, 2, 3, 4, 5]
+# result = reverse_list(my_list)
+# print("Reversed list:", result)
+# max_value = max(result)
+# print("Maximum element in the reversed list:", max_value)
+# print(result[1])
+
+a = 0
+b = 10
+
+
+temp = a
+a = b
+b=a
+
+print(a)  # Output will be: 10 0
+
+
 
