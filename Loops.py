f5117779195410a86d0937185dd67ffc055a46ca
Shoyebaktar Shirol <shoyebaktarshirol@gmail.com>
2024-03-21 18:23:11 +0530

    Palindrome

--- Loops.py
+++ Loops.py
@@ -100,3 +100,16 @@
 # else:
 #     print("Not A Palindrome")
 
+def DFS(graph, start):
+    visited = set()
+    stack = [start]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            stack.extend(set(graph[vertex]) - visited)
+            return visited
+graph = { 'A': ['B', 'C'], 'B': ['D', 'E'], 'C': ['F'], 'D': [], 'E': ['F'], 'F': []}
+visited = set()
+print("The Following result of DFS \n")
+print(DFS(graph,'A'))
