61dc93744144a82b798065ee5ada232f7088706e
Shoyebaktar Shirol <shoyebaktarshirol@gmail.com>
2024-05-01 16:43:17 +0530

    Cunstructors

--- I/I/oops.py
+++ I/I/oops.py
@@ -52,19 +52,15 @@
 # obj = Student("john")
 # print(obj.name)
 
-#DEFAULT CUNSTRUCTOR
-class Shoyeb:
-        def __init__(self):
-                print("Hello I am Learning cunstructor in Oops\nA Cunstructor is automatically called when an object is created.")
+#DEFAULT CUNSTRUCTOR And Self is keyword use it mendatory otherwise we get Error
+# class Shoyeb:
+#         def __init__(self):
+#                 print("Hello I am Learning cunstructor in Oops\nA Cunstructor is automatically called when an object is created.")
 
-obj2 = Shoyeb()
-
-
+# obj2 = Shoyeb()
 
 
-
-
-
+#PARAMETERISED CUNSTRUCTOR
 # class demo:
      
 #         def __init__(self,lastName):
@@ -74,11 +70,57 @@ obj2 = Shoyeb()
 # obj1 = demo("JACK MA")
 # print(obj1.name)
 
+#Method and Class
+
+# class Student :
+#         collge = "KLE BCA GOKAK"
+
+#         def __init__(self):
+#                 self.name = name 
+#                 self.lastname = lastName
+
+#         def welcome(self):
+#                 print("Hello Methods")
+# s1 = Student("SHoyebaktar","shirol")
+# s1.welcome()
 
 
-       
+
+# class Student:
+#         def __init__(self,name,marks):
+#             self.name = name 
+#             self.marks = marks
+        
+
+#         @staticmethod  # There is no self parameter i static methd work at the class lavel
+#         def show():
+#                 print("Hello World")
+#                 print("this is the static method ")
 
 
+#         def get_avg(self):
+#                 sum = 0
+#                 for val in self.marks:
+#                         sum += val
+#                         print("Hi",self.name,"Ur  Avg Score is:",sum/3)
+
+# s1 = Student("Tony",[95,87,65])
+# s1.get_avg()
+# s1.show()
+
+
+class demo:
+
+
+    @staticmethod
+    def hello():
+        print("This is a static method inside a class")
+        print("I created indide the class ")
+        print("Ststic method works for class level only")
+
+s1 = demo()
+
+demo.hello()  # Correct way to call the static method
 
 
 
